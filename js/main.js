28b342c5b463027161b15ee3ef4bc12829b85fe0
Alexey <Andreenko7@gmail.com>
2020-01-24 13:07:27 +0200

    got the value of the birth day

---
+++ js/main.js
@@ -0,0 +1,112 @@
+$(document).ready(function () {
+  // finding index of a birth day:
+  $('#birthDay1').click(function (e) {
+    $('#dayStatus').html($('#birthDay1').text());
+  });
+  $('#birthDay2').click(function (e) {
+    $('#dayStatus').html($('#birthDay2').text());
+  });
+  $('#birthDay3').click(function (e) {
+    $('#dayStatus').html($('#birthDay3').text());
+  });
+  $('#birthDay4').click(function (e) {
+    $('#dayStatus').html($('#birthDay4').text());
+  });
+  $('#birthDay5').click(function (e) {
+    $('#dayStatus').html($('#birthDay5').text());
+  });
+  $('#birthDay6').click(function (e) {
+    $('#dayStatus').html($('#birthDay6').text());
+  });
+  $('#birthDay7').click(function (e) {
+    $('#dayStatus').html($('#birthDay7').text());
+  });
+  $('#birthDay8').click(function (e) {
+    $('#dayStatus').html($('#birthDay8').text());
+  });
+  $('#birthDay9').click(function (e) {
+    $('#dayStatus').html($('#birthDay9').text());
+  });
+  $('#birthDay10').click(function (e) {
+    $('#dayStatus').html($('#birthDay10').text());
+  });
+  $('#birthDay11').click(function (e) {
+    $('#dayStatus').html($('#birthDay11').text());
+  });
+  $('#birthDay12').click(function (e) {
+    $('#dayStatus').html($('#birthDay12').text());
+  });
+  $('#birthDay13').click(function (e) {
+    $('#dayStatus').html($('#birthDay13').text());
+  });
+  $('#birthDay14').click(function (e) {
+    $('#dayStatus').html($('#birthDay14').text());
+  });
+  $('#birthDay15').click(function (e) {
+    $('#dayStatus').html($('#birthDay15').text());
+  });
+  $('#birthDay16').click(function (e) {
+    $('#dayStatus').html($('#birthDay16').text());
+  });
+  $('#birthDay17').click(function (e) {
+    $('#dayStatus').html($('#birthDay17').text());
+  });
+  $('#birthDay18').click(function (e) {
+    $('#dayStatus').html($('#birthDay18').text());
+  });
+  $('#birthDay19').click(function (e) {
+    $('#dayStatus').html($('#birthDay19').text());
+  });
+  $('#birthDay20').click(function (e) {
+    $('#dayStatus').html($('#birthDay20').text());
+  });
+  $('#birthDay21').click(function (e) {
+    $('#dayStatus').html($('#birthDay21').text());
+  });
+  $('#birthDay22').click(function (e) {
+    $('#dayStatus').html($('#birthDay22').text());
+  });
+  $('#birthDay23').click(function (e) {
+    $('#dayStatus').html($('#birthDay23').text());
+  });
+  $('#birthDay24').click(function (e) {
+    $('#dayStatus').html($('#birthDay24').text());
+  });
+  $('#birthDay25').click(function (e) {
+    $('#dayStatus').html($('#birthDay25').text());
+  });
+  $('#birthDay26').click(function (e) {
+    $('#dayStatus').html($('#birthDay26').text());
+  });
+  $('#birthDay27').click(function (e) {
+    $('#dayStatus').html($('#birthDay27').text());
+  });
+  $('#birthDay28').click(function (e) {
+    $('#dayStatus').html($('#birthDay28').text());
+  });
+  $('#birthDay29').click(function (e) {
+    $('#dayStatus').html($('#birthDay29').text());
+  });
+  $('#birthDay30').click(function (e) {
+    $('#dayStatus').html($('#birthDay30').text());
+  });
+  $('#birthDay31').click(function (e) {
+    $('#dayStatus').html($('#birthDay31').text());
+  });
+
+
+
+
+
+
+
+
+  // taking birth day index and put their to variable 'birthDay':
+  $('#getResult').click(function (e) {
+    e.preventDefault();
+    var birthDay = $('#dayStatus').text();
+    alert(birthDay + " Ах ты долбоеб");
+  });
+
+});
+
